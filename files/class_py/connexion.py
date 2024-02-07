fbd558c7f223baf0952d9aea0197b227377ca5a4
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-02-07 17:20:57 +0100

    font de la page d'accueil terminé

--- files/class_py/connexion.py
+++
@@ -1,5 +0,0 @@
-from files.class_py.database import Database
-
-class Connexion:
-    def __init__(self):
-        self.database = Database()
--- files/class_py/inscription.py
+++
@@ -1,8 +0,0 @@
-from files.class_py.database import Database
-
-class Inscription:
-    def __init__(self):
-        self.database = Database()
-        
-        
-        
