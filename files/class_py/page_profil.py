a5d490a9615701dff229d4e544c730b964aed6cc
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-02-09 15:52:21 +0100

    changement position du rect serveur dans page profil

--- files/class_py/page_profil.py
+++ files/class_py/page_profil.py
@@ -19,7 +19,7 @@ class PageProfil(Interface):
         
     def rect_server(self):
         # Zone des serveurs
-        self.solid_rect((64, 68, 75), 30, 100, 1000, 90)
+        self.solid_rect((64, 68, 75), 30, 100, 90, 1000)
         
     def create_server(self):
         # Coordonnées du cercle
