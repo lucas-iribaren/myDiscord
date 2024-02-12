52f183a6e7119461d6d7ea8a50d3159961693d70
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-02-12 12:59:15 +0100

    ajout de la class user avec les fonction CRUD

---
+++ files/class_py/user.py
@@ -0,0 +1,17 @@
+from files.class_py.database import Database
+
+class User(Database):
+    def __init__(self):
+        Database.__init__(self)
+        
+    def add_user(self, pseudo, mail, password, id_categorie):
+        sql = "INSERT INTO user (pseudo, mail, password, id_categorie) VALUES (%s, %s, %s, %s);"
+        self.execute_sql(sql, (pseudo, mail, password, id_categorie))
+        
+    def update_user(self, user_id, new_pseudo, new_mail, new_password, new_id_categorie):
+        sql = "UPDATE user SET pseudo = %s, mail = %s, password = %s, id_categorie = %s WHERE id = %s;"
+        self.execute_sql(sql, (new_pseudo, new_mail, new_password, new_id_categorie, user_id))
+
+    def delete_user(self, user_id):
+        sql = "DELETE FROM user WHERE id = %s;"
+        self.execute_sql(sql, (user_id,))
