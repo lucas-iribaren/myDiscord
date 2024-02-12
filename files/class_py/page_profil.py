9b58551bda041e276b80cd0adfbd9a5be8811db1
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-02-12 16:43:57 +0100

    Modification des commentaires

--- files/class_py/page_profil.py
+++ files/class_py/page_profil.py
@@ -5,16 +5,14 @@ from files.class_py.interface import Interface
 class PageProfil(Interface):
     def __init__(self):
         super().__init__()  # Appelle le constructeur de la classe parente
-        self.profil_run = False  # Initialiser profil_run à False pour entrer dans la boucle principale
+        self.profil_run = False  # Initialise profil_run à False pour entrer dans la boucle principale
 
     def create_profile_page(self):
-        # Remplir l'écran avec du gris
+        # Remplit l'écran avec du gris
         self.Screen.fill((54, 57, 63))
 
-        # Ajout du logo
+        # Ajout du logo au milieu de la fenêtre
         self.img(550, 270, 100, 100, "icones/logo")
-
-        # Texte à afficher
         self.text(25, "Veuillez choisir un serveur.", (249, 249, 249), 450, 320)
         
     def rect_server(self):
@@ -22,37 +20,58 @@ class PageProfil(Interface):
         self.solid_rect((64, 68, 75), 0, 0, 70, 1000)
         
     def create_server(self):
-        # Coordonnées du cercle
+        # Coordonnées du bouton "Créer un serveur"
         circle_center = (35, 35)
         circle_radius = 28
         
-        # Vérifier si la souris survole le cercle
+        # Vérifie si la souris survole le bouton
         mouse_pos = pygame.mouse.get_pos()
         distance_to_circle = ((mouse_pos[0] - circle_center[0])**2 + (mouse_pos[1] - circle_center[1])**2) ** 0.5
         
         if distance_to_circle <= circle_radius:
-            # Survole du cercle : change la couleur du bouton
+            # Survol du cercle - Change la couleur du bouton
             pygame.draw.circle(self.Screen, (114, 137, 218), circle_center, circle_radius + 2)  # Cercle
-            # Dessin de la croix
-            pygame.draw.line(self.Screen, (188, 186, 184), (20, 35), (58, 35), 3)  # Ligne horizontale
-            pygame.draw.line(self.Screen, (188, 186, 184), (39, 55), (39, 15), 3)  # Ligne verticale
-            self.text(20, "Créer un serveur", (249, 249, 249), 40, 30)
+            # Dessin de la croix - Survol
+            pygame.draw.line(self.Screen, (188, 186, 184), (15, 35), (53, 35), 3)  # Ligne horizontale
+            pygame.draw.line(self.Screen, (188, 186, 184), (35, 55), (35, 15), 3)  # Ligne verticale
+            self.solid_rect((0, 0, 0), 70, 20, 100, 50) # Zone de texte au survol du bouton
+            self.text(20, "Créer un serveur", (249, 249, 249), 70, 20)
         else:
             # Sans survol
-            pygame.draw.circle(self.Screen, (188, 186, 184), circle_center, circle_radius)  # Cercle normal
-            # Dessin de la croix
-            pygame.draw.line(self.Screen, (114, 137, 218), (20, 35), (58, 35), 3)  # Ligne horizontale
-            pygame.draw.line(self.Screen, (114, 137, 218), (39, 55), (39, 15), 3)  # Ligne verticale
+            pygame.draw.circle(self.Screen, (188, 186, 184), circle_center, circle_radius)
+            # Dessin de la croix - Sans survol
+            pygame.draw.line(self.Screen, (114, 137, 218), (15, 35), (53, 35), 3)  # Ligne horizontale
+            pygame.draw.line(self.Screen, (114, 137, 218), (35, 55), (35, 15), 3)  # Ligne verticale
+
+    def private_server(self):
+        # Coordonnées du bouton "Serveurs privé"
+        circle_center = (35, 100)
+        circle_radius = 28
+
+        # Vérifie si la souris survole le cercle
+        mouse_pos = pygame.mouse.get_pos()
+        distance_to_circle = ((mouse_pos[0] - circle_center[0])**2 + (mouse_pos[1] - circle_center[1])**2) ** 0.5
+
+        if distance_to_circle <= circle_radius:
+            # Survol du cercle - Change la couleur de l'icone
+            pygame.draw.circle(self.Screen, (114, 137, 218), circle_center, circle_radius + 2)
+            self.img(35, 100, 50, 50, "icones/avatar_2")
+            self.solid_rect((0, 0, 0), 70, 50, 100, 50) # Zone de texte au survol du bouton
+            self.text(20, "Serveur privé", (249, 249, 249), 90, 100)
+        else:
+            # Sans survol
+            self.img(35, 100, 50, 50, "icones/avatar_0")
 
     def home_profil(self):
         self.profil_run = True
         while self.profil_run:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.profil_run = False  # Sortir de la boucle lorsque l'événement QUIT est détecté
+                    self.profil_run = False  # Sort de la boucle lorsque l'événement QUIT est détecté
                     pygame.quit()
 
             self.create_profile_page()
             self.rect_server()
             self.create_server()
+            self.private_server()
             self.update()  
