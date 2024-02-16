2d98c8a1c7782a404613b4a7fff8a1cf3cb101c8
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-02-16 13:27:33 +0100

    Changement des commentaires en anglais

--- files/class_py/profil.py
+++ files/class_py/profil.py
@@ -5,81 +5,81 @@ from files.class_py.notification import Notification
 
 class Profil(Interface):
     def __init__(self, user):
-        super().__init__()  # Appelle le constructeur de la classe parente
-        self.profil_run = False  # Initialise profil_run à False pour entrer dans la boucle principale
+        super().__init__()  # Call the constructor of the parent class 
+        self.profil_run = False  # Initialize profil_run to False to enter the main loop
         self.private_chanels = False
         self.user = user
         self.channel_message = "Veuillez choisir un serveur."
         self.message = Message(self.user)
         self.notification = Notification(self.user)
-        self.active_input = None  # Pour suivre le champ de texte actif
+        self.active_input = None  # Follow the active text field
         self.input_texts_message = {'message':''}        
 
     def create_profile_page(self):
-        # Remplit l'écran avec du gris
+        # Fill the screen in gray
         self.Screen.fill((54, 57, 63))
 
-        # Ajout du logo au milieu de la fenêtre
+        # Add a logo
         self.img(550, 270, 100, 100, "icones/logo")
         self.text(25, self.channel_message, (249, 249, 249), 435, 320)
 
-        # Dessine la zone des channels privés
+        # Draw channels area
         self.rect_pv_chanel()
         
     def rect_server(self):
-        # Zone des serveurs
+        # Servers area
         self.solid_rect((64, 68, 75), 0, 0, 70, 1000)
         
     def create_server(self):
-        # Coordonnées du bouton "Créer un serveur"
+        # Coordonates of "Créer un serveur" 
         circle_center = (35, 35)
         circle_radius = 28
         
-        # Vérifie si la souris survole le bouton
+        # Verify if the mouse is hover the button
         mouse_pos = pygame.mouse.get_pos()
         distance_to_circle = ((mouse_pos[0] - circle_center[0])**2 + (mouse_pos[1] - circle_center[1])**2) ** 0.5
         
         if distance_to_circle <= circle_radius:
-            # Survol du cercle - Change la couleur du bouton
-            pygame.draw.circle(self.Screen, (114, 137, 218), circle_center, circle_radius + 2)  # Cercle
-            # Dessin de la croix - Survol
-            pygame.draw.line(self.Screen, (188, 186, 184), (15, 35), (53, 35), 3)  # Ligne horizontale
-            pygame.draw.line(self.Screen, (188, 186, 184), (35, 55), (35, 15), 3)  # Ligne verticale
+            # Hoover of the circle - Change the color
+            pygame.draw.circle(self.Screen, (114, 137, 218), circle_center, circle_radius + 2)  # Circle
+            # Draw the cross - Hoover
+            pygame.draw.line(self.Screen, (188, 186, 184), (15, 35), (53, 35), 3)  # Horizontal line
+            pygame.draw.line(self.Screen, (188, 186, 184), (35, 55), (35, 15), 3)  # Vertical line
             self.img(130, 30, 130, 40, "icones/zone_texte_survol")
             self.text(20, "Créer un serveur", (249, 249, 249), 80, 20)
         else:
-            # Sans survol
+            # Wihtout hoover
             pygame.draw.circle(self.Screen, (188, 186, 184), circle_center, circle_radius)
-            # Dessin de la croix - Sans survol
-            pygame.draw.line(self.Screen, (114, 137, 218), (15, 35), (53, 35), 3)  # Ligne horizontale
-            pygame.draw.line(self.Screen, (114, 137, 218), (35, 55), (35, 15), 3)  # Ligne verticale
+            # Draw cross - without hoover
+            pygame.draw.line(self.Screen, (114, 137, 218), (15, 35), (53, 35), 3)  # Horizontal line
+            pygame.draw.line(self.Screen, (114, 137, 218), (35, 55), (35, 15), 3)  # Vertical line
 
     def private_server(self):
-        # Coordonnées du bouton "Serveurs privé"
+        # Coordonate button : "Messages privé"
         circle_center = (35, 100)
         circle_radius = 28
 
-        # Vérifie si la souris survole le cercle
+        # Verify if the mouse is hover the circle
         mouse_pos = pygame.mouse.get_pos()
         distance_to_circle = ((mouse_pos[0] - circle_center[0])**2 + (mouse_pos[1] - circle_center[1])**2) ** 0.5
 
         if distance_to_circle <= circle_radius:
-            # Survol du cercle - Change la couleur de l'icone
+            # Hoover of the circle - Change the color of the icon
             pygame.draw.circle(self.Screen, (114, 137, 218), circle_center, circle_radius + 2)
             self.img(35, 100, 50, 50, "icones/avatar_2")
-            self.img(130, 100, 140, 40, "icones/zone_texte_survol") # Zone texte directionnel
+            self.img(130, 100, 140, 40, "icones/zone_texte_survol") # Text area
             self.text(20, "Messages privés", (249, 249, 249), 85, 90)
 
-            # Vérifie si le bouton a été cliqué
+            # Verify if the mouse is cliqued
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button ==  1:
-                    self.private_chanels = not self.private_chanels  # Bascule l'affichage de la zone des channels privés
+                    self.private_chanels = not self.private_chanels  # Toggle the display of the private channels area
                     # if self.private_chanels:
                     #     self.channel_message = "Veuillez sélectionner un channel." # Change le message lorsque le bouton est cliqué
                     # else:
                     #     self.channel_message = "Veuillez choisir un serveur." # Rétablit le message par défaut lorsque le bouton est cliqué à nouveau
         else:
-            # Sans survol
+            # Without hoover
             self.img(35, 100, 50, 50, "icones/avatar_0")
             
     def event_writting_message(self):
@@ -100,9 +100,12 @@ class Profil(Interface):
                 
 
     def rect_pv_chanel(self):
-        # Dessine la zone des channels privés
+        # Draw private channels area
         if self.private_chanels:
-            self.solid_rect((64, 68, 75), 80, 0, 90, 1000)
+            self.solid_rect((64, 68, 75), 80, 0, 100, 1000)
+            self.solid_rect((54, 55, 61), 80, 0, 100, 30)
+            self.solid_rect_radius((0, 0, 0), 80, 0, 100, 30, 5)
+            self.text(20, "Channels", (249, 249, 249), 85, 90)
     
 
     def home_profil(self):
@@ -110,7 +113,7 @@ class Profil(Interface):
         while self.profil_run:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.profil_run = False  # Sort de la boucle lorsque l'événement QUIT est détecté
+                    self.profil_run = False  # Exit the loop when the QUIT event is detected
 
             self.create_profile_page()
             self.rect_server()
